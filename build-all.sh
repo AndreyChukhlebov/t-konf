8b0106375f90d5241874de89b6ec4948fd68f631
andrey <an.chukhlebov@gmail.com>
2025-10-17 19:54:22 +0300

    init commit

---
+++ build-all.sh
@@ -0,0 +1,3 @@
+(cd tkonf-app-micronaut && ./mvnw clean install -Pdocker-tests)
+(cd tkonf-app-quarkus && ./mvnw clean install -Pdocker-tests)
+(cd tkonf-app-spring && ./mvnw clean install -Pdocker-tests)
